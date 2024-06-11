488ed52f575993a1e4323f5557239e860fafbea1
SirEvgen <sandrofa6666666@gmail.com>
2024-06-11 17:32:34 +0300

    Update module3_hard.py

--- module3_hard.py
+++ module3_hard.py
@@ -1,33 +1,30 @@
 def calculate_structure_sum(list_):
     total_sum = 0
     total_len = 0
-
-    for item in list_:
-        if isinstance(item, list):
-            total_sum += sum(item)
-        elif isinstance(item, dict):
-            for key, value in item.items():
-                total_sum += calculate_structure_sum(value)
-                if isinstance(key, str):
-                    total_len += len(key)
-        elif isinstance(item, tuple):
-            for dict_ in item:
-                if isinstance(dict_, dict):
-                    for key, value in dict_:
-                        total_sum += calculate_structure_sum(value)
-                        if isinstance(key, str):
-                            total_len += len(key)
-                for dict2 in dict_:
-                    if isinstance(dict2, list):
-                        for key, value in dict_.items():
+    if isinstance(list_, list) or isinstance(list_, (dict, tuple, str)):
+        for item in list_:
+            if isinstance(item, list):
+                total_sum += sum(item)
+            elif isinstance(item, dict):
+                for key, value in item.items():
+                    total_sum += calculate_structure_sum(value)
+                    if isinstance(key, str):
+                        total_len += len(key)
+            elif isinstance(item, tuple):
+                for dict_ in item:
+                    if isinstance(dict_, dict):
+                        for key, value in dict_:
                             total_sum += calculate_structure_sum(value)
                             if isinstance(key, str):
                                 total_len += len(key)
-        elif isinstance(item, str):
-            total_len += len(item)
-
-
-
+                    for dict2 in dict_:
+                        if isinstance(dict2, list):
+                            for key, value in dict_.items():
+                                total_sum += calculate_structure_sum(value)
+                                if isinstance(key, str):
+                                    total_len += len(key)
+            elif isinstance(item, str):
+                total_len += len(item)
     return total_sum, total_len
 
 
